567a2f4075dad3e490601c808998be71fae6bd44
cykreet <cykreet@gmail.com>
2025-02-21 14:06:52 +0200

    feat: connection validation and node parse start

--- src/app.tsx
+++ src/app.tsx
@@ -7,18 +7,22 @@ import {
 	ConnectionMode,
 	Controls,
 	ReactFlow,
+	SelectionMode,
+	type Connection,
 	type Edge,
+	type EdgeRemoveChange,
 	type Node,
 	type OnConnect,
 	type OnEdgesChange,
 	type OnNodesChange,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { PellaView } from "./components/pella-view/pella-view";
-import { TagNode } from "./components/tag-node/tag-node";
+import { TagNode, type TagNodeProps } from "./components/tag-node/tag-node";
 import { TagSelector } from "./components/tag-selector/tag-selector";
-import { atlasTags } from "./tags";
+import { atlasTags, PellaExecutionType, type PellaTag } from "./tags";
+import { parsePellaNode } from "./helpers/parse-pella-node";
 
 export const App = () => {
 	const [nodes, setNodes] = useState<Node[]>([]);
@@ -33,17 +37,56 @@ export const App = () => {
 	const onConnect: OnConnect = useCallback(
 		(connection) =>
 			setEdges((eds) => {
-				const newEdges = addEdge({ ...connection, type: ConnectionLineType.SmoothStep }, eds);
-				setOutputLines(
-					newEdges.map((edge) => {
-						return edge.target;
-					}),
+				let newEdges = new Array<Edge>(...eds);
+				// remove previous edges if new edges are connected to the same handles
+				// i.e, only a single edge from an individual handle
+				const presentEdge = newEdges.find(
+					(edge) => edge.sourceHandle === connection.sourceHandle && edge.source === connection.source,
 				);
+				if (presentEdge) {
+					const edgeRemoveChange: EdgeRemoveChange = { id: presentEdge.id, type: "remove" };
+					newEdges = applyEdgeChanges([edgeRemoveChange], newEdges);
+				}
 
-				return newEdges;
+				const outEdges = addEdge({ ...connection, type: ConnectionLineType.SmoothStep }, newEdges);
+				return outEdges;
 			}),
 		[],
 	);
+
+	useEffect(() => {
+		const executionNodes = edges
+			.reduce((acc, edge) => {
+				const sourceNode = nodes.find((node) => node.id === edge.source);
+				if (sourceNode != null && !acc.includes(sourceNode)) acc.push(sourceNode);
+				const targetNode = nodes.find((node) => node.id === edge.target);
+				if (targetNode != null && !acc.includes(targetNode)) acc.push(targetNode);
+				return acc;
+			}, new Array<Node>())
+			.filter((node): node is Node & { data: PellaTag } => {
+				const nodeData = node.data as unknown as PellaTag;
+				if (nodeData == null) return false;
+				if (nodeData.executionType === PellaExecutionType.Function) return true;
+				if (nodeData.executionType === PellaExecutionType.Setter) return true;
+				return false;
+			});
+
+		setOutputLines(executionNodes.map((node) => parsePellaNode(node)));
+	}, [edges, nodes]);
+
+	const validateConnection = (edge: Connection | Edge): boolean => {
+		const sourceNode = nodes.find((node) => node.id === edge.source);
+		const targetNode = nodes.find((node) => node.id === edge.target);
+		const sourceParameter = (sourceNode?.data as PellaTag | undefined)?.outputParameters?.find(
+			(parameter) => parameter.name === edge.sourceHandle,
+		);
+
+		const targetParameter = (targetNode?.data as PellaTag | undefined)?.inputParameters?.find(
+			(parameter) => parameter.name === edge.targetHandle,
+		);
+
+		return sourceParameter?.type === targetParameter?.type;
+	};
 
 	return (
 		<div style={{ height: "100%" }}>
@@ -53,6 +96,7 @@ export const App = () => {
 				nodeTypes={nodeTypes}
 				connectionMode={ConnectionMode.Strict}
 				connectionLineType={ConnectionLineType.SmoothStep}
+				isValidConnection={validateConnection}
 				selectionOnDrag={true}
 				panOnDrag={[1]} // pan on middle mouse drag [0, 1, 2]
 				snapToGrid={true}
@@ -60,6 +104,7 @@ export const App = () => {
 				edges={edges}
 				onNodesChange={onNodesChange}
 				onEdgesChange={onEdgesChange}
+				selectionMode={SelectionMode.Partial}
 				onMove={() => setContextOpen(false)}
 				onContextMenu={(event) => {
 					event.preventDefault();
